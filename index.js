f684cedde6c1ab95d8436bd222ac1110aa4a7003
Joseph Omitogun <joseyi2013@gmail.com>
2019-09-22 14:15:35 +0100

    updated index,js and package.json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const app = express();
 app.use(express.static(__dirname+"/UI"))
+app.use(express.json());
 const port=process.env.PORT||3000;
 const userList=[];
 
@@ -8,12 +9,28 @@ const userList=[];
 app.get("/",(req,res)=>{
     res.render("index");
 });
-app.get("/api/users",(req,res)=>{
+app.get("/api/v1/users",(req,res)=>{
     res.send(userList);
 });
-app.get("/api/users/:userName",(req,res)=>{
+app.get("/api/v1/users/:userName",(req,res)=>{
     let user=userList.find(e=>e.userName===req.params.userName);
     if(user) return res.send(user);
     return res.status(404).send("user not found");
 });
-app.listen(port,()=>console.log("sever running at "+port))
+app.get("/api/v1/users",(req,res)=>{
+    const course={
+        
+            firstName : req.body.firstName ,
+            lastName : req.body.lastName,
+            email : req.body.email,
+            userName:req.body.userName,
+            password : req.body.password,
+            gender : req.body.gender,
+            jobRole : req.body.jobRole ,
+            department : req.body.department ,
+            address : req.body.address
+                }
+    userList.push(course);
+    res.status(201).send(course);
+});
+app.listen(port,()=>console.log("sever running at "+port));
